76e7b4401e94e45b9e1a1466f0af686c42ea5856
vlad <vlad.genyuk@mail.ru>
2023-10-28 17:40:20 +0300

    test_deploy_version

--- social/social/settings.py
+++ social/social/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v%u1^i@^%0x(r&3e5o06&elul68jtihaverkw2$ucuoa7_dh-n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '*', 'iatsun.onrender.com']
 
